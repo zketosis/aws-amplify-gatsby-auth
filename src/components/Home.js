3b84b72cc6a2b7357ff3ca034f9c20acf5f59f95
Zed Zaxter <ketosis@gmail.com>
2020-05-10 16:53:16 -0500

    Set some extra info and see if it builds.
    extra comments.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Home = () => <div>
-  <h1>Home</h1>
-	<p>You are now logged in! <Link to="/app/profile">View profile</Link></p>
+  <h1>So you are Home</h1>
+	<p>You IS now logged in! <Link to="/app/profile">View profile</Link></p>
+	<p><Link to="/app/profile">another link to View profile</Link></p>
 	<p>Now go build something great and deploy it using the <a href="https://console.amplify.aws">AWS Amplify Console</a></p>
 	</div>
 
