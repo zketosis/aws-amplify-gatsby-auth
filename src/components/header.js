de5aa74905905a91eab7165d5da9394d483d6609
Zed Zaxter <ketosis@gmail.com>
2020-05-10 17:07:21 -0500

    lower the overhead!

--- src/components/header.js
+++ src/components/header.js
@@ -9,15 +9,15 @@ import { Auth } from 'aws-amplify'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: '#FF9900',
+      marginBottom: '0.45rem',
     }}
   >
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: '0.45rem 0.0875rem',
       }}
     >
       <h1 style={{ margin: 0 }}>
